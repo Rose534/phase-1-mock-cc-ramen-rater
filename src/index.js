45f975e7e83ff7fe89843a925db6d95fcc11431d
rose534 <wambuirose534@gmail.com>
2022-12-10 00:03:52 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-// write your code here
+const API = "http://localhost:3000/ramens"
+
+el('new-ramen').addEventListener('submit', createNewRamen)
+const ramenMenuDiv = document.getElementById('ramen-menu');
+fetch(API)
+  .then(res => res.json())
+  .then(renderRamens);
+
+function renderRamens(ramens) {
+    console.log(ramens);
+
+ramens.forEach(renderRamen)
+}
+
+function renderRamen(ramen) {
+    console.log(ramenMenuDiv)
+
+const ramenImage = document.createElement('img');
+ramenImage.src = ramen.image;
+ramenMenuDiv.append(ramenImage);
+
+ramenImage.addEventListener("click", e => renderDetails(ramen))
+}
+
+function renderDetails(ramen) {
+    console.log(ramen.image)
+    const ramenDetail = document.getElementById('ramen-detail');
+    const detailImage = el('detail-image')
+    const ramenName = el('ramen-name')
+    const restaurantName = el('restaurant-name')
+    const ratingDisplay = el("rating-display")
+    const commentDisplay = el("comment-display")
+
+    detailImage.src = ramen.image;
+    detailImage.alt = ramen.name;
+    ramenName.textContent = ramen.name;
+    restaurantName.textContent = ramen.restaurant;
+    ratingDisplay.textContent = ramen.rating;
+    commentDisplay.textContent = ramen.comment;
+}
+function createNewRamen(e){
+    e.preventDefault();
+    console.log(e.target.name.value)
+    const newRamen = {
+        name: e.target.name.value,
+        rating: e.target.rating.value,
+        restaurant: e.target.restaurant.value,
+        image: e.target.image.value,
+        comment: e.target['new-comment'].value,
+    }
+    renderRamen(newRamen);
+    e.target.clear();
+}
+
+
+function el(elementName) {
+    return document.getElementById(elementName)
+}
+
+
+
+
